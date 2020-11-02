6567979cd374b51e7913c659f8432fa882d33a93
youexamplecom <rrr83u@example.com>
2020-11-02 13:54:38 +0530

    this is code for reverse an array in brute forces method

---
+++ src/NewClass.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class NewClass{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int ar[]=new int[3];
+        for(int i=0;i<3;i++){
+            ar[i]=sc.nextInt();
+        }
+        
+        
+    }
+}
